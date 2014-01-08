460be391203c33729d75d4938774472ca463e35c
Eliakim Aquino <eliakim.aquino@gmail.com>
2014-01-07 21:35:55 -0300

    CRUD de Servicos

--- application/models/DbTable/Servicos.php
+++ application/models/DbTable/Servicos.php
@@ -12,4 +12,34 @@ class Application_Model_DbTable_Servicos extends Zend_Db_Table_Abstract{
 		
 		parent::insert($data);
 	}
+	
+	
+	public function editar($id){
+		
+		$sql = $this->select()->where('idServicos = ?', $id);
+		$row = $this->fetchRow($sql);
+		
+		if(null !== $row)
+		return $row->toArray();
+	}
+	
+	
+	public function deletar($id){
+		
+		$sql = $this->select()->where('idServicos = ?', $id);
+		
+		$row = $this->fetchRow($sql);
+		
+		$row->delete();
+		
+	}
+	
+	public function listar(){
+		
+		$sql = $this->select()->order('nome ASC');
+		
+		$rows = $this->fetchAll($sql);
+		
+		return $rows;
+	}
 }
--- application/modules/admin/controllers/ServicosController.php
+++ application/modules/admin/controllers/ServicosController.php
@@ -18,6 +18,46 @@ class Admin_ServicosController extends Zend_Controller_Action{
 		}
 		
 		$this->view->form = $form;
+		
+		$this->view->rows = $model->listar();
+	}
+	
+	
+	public function editarAction(){
+		
+		$model = new Application_Model_DbTable_Servicos();
+		$form = new Application_Form_Servicos();
+		
+		$id = $this->_getParam('idServicos');
+		
+		$data = $model->editar($id);
+		if(is_array($data)){
+			$form->populate($data);
+		}
+		
+		if($this->_request->isPost()){
+			if($form->isValid($this->_request->getPost())){
+				$data = $form->getValues();
+				if($id){
+					$where = $model->getAdapter()->quoteInto('idServicos = ?', $id);
+					$model->update($data, $where);
+					$this->_redirect("/admin/servicos");
+				}
+			}
+		}
+		$this->view->form = $form;
+	}
+	
+	
+	public function deleteAction(){
+		
+		$model = new Application_Model_DbTable_Servicos();
+		
+		$id = $this->_getParam('idServicos');
+		
+		$model->deletar($id);
+		
+		$this->_redirect("/admin/servicos");
 	}
 	
 }
